bea89461e48109a543a569bed289afb2e265f226
Gabriel <bristotgl@gmail.com>
2026-05-12 15:59:28 -0300

    adds metrics middleware and related endpoints

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -9,7 +24,34 @@ func main() {
 		Addr:    ":8080",
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	apiCfg := apiConfig{atomic.Int32{}}
+
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(
+		http.FileServer(http.Dir(".")),
+	)),
+	)
+	mux.HandleFunc("/healthz", HealthzHandler)
+	mux.HandleFunc("/metrics", apiCfg.MetricsHandler)
+	mux.HandleFunc("/reset", apiCfg.ResetHandler)
 
 	server.ListenAndServe()
+}
+
+func HealthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) MetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) ResetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+
+	cfg.fileserverHits.Store(0)
 }
